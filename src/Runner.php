02f08b2b611896f76ce954d6db6fd9e3366f2a91
Dejan Samardzija <samardzija.dejan@gmail.com>
2014-05-16 19:08:18 +0200

    initial work done, runner is operational, now comes the interesting part :)

--- src/Runner.php
+++ src/Runner.php
@@ -2,16 +2,67 @@
 
 namespace G4\Runner;
 
-class Runner
-{
-    public function bootstrap()
-    {
+use G4\CleanCore\Application;
+use G4\DI\Container as DI;
 
+abstract class Runner implements RunnerInterface
+{
+    /**
+     * @var G4\Http\Request
+     */
+    protected $httpRequest;
+
+    /**
+     * @var G4\Http\Response
+     */
+    protected $httpResponse;
+
+    public final function bootstrap()
+    {
+        $this->_bootstrap();
+        return $this;
     }
 
-    public function run()
-    {
+    abstract protected function _bootstrap();
 
+    public final function run()
+    {
+        $this->_run();
+        return $this;
+    }
+
+    abstract protected function _run();
+
+    public function getHttpRequest()
+    {
+        if(null === $this->httpRequest) {
+            $this->httpRequest = DI::get('HttpRequest');
+        }
+        return $this->httpRequest;
+    }
+
+    public function getHttpResponse()
+    {
+        if(null === $this->httpResponse) {
+            $this->httpResponse = DI::get('HttpResponse');
+        }
+        return $this->httpResponse;
+    }
+
+    public function getApplicationModule()
+    {
+    }
+
+    public function getApplicationService()
+    {
+    }
+
+    public function getApplicationMethod()
+    {
+    }
+
+    public function getApplicationParams()
+    {
     }
 
 }
