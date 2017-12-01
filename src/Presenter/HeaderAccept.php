88e06db974b3ff54fbc82b5130291fc23f2fa6a6
Drasko Gomboc <drasko.gomboc@gmail.com>
2017-12-01 11:47:13 +0100

    HeaderAccept refactoring; map text/* to twig format

--- src/Presenter/HeaderAccept.php
+++ src/Presenter/HeaderAccept.php
@@ -5,11 +5,23 @@ namespace G4\Runner\Presenter;
 use Aura\Accept\Accept;
 use Aura\Accept\AcceptFactory;
 use Aura\Accept\Media\MediaValue;
-use G4\Constants\Format;
-use G4\Constants\HeaderAccept as HeaderAcceptConst;
 
 class HeaderAccept
 {
+    const FORMAT_HTML = 'html';
+    const FORMAT_JSON = 'json';
+    const FORMAT_TWIG = 'twig';
+    const FORMAT_XML  = 'xml';
+
+    const ACCEPT_HTML = 'text/html';
+    const ACCEPT_TEXT = 'text/*';
+    const ACCEPT_JSON = 'application/json';
+
+    const ACCEPT_MAP = [
+        self::ACCEPT_JSON => self::FORMAT_JSON,
+        self::ACCEPT_HTML => self::FORMAT_TWIG,
+        self::ACCEPT_TEXT => self::FORMAT_TWIG,
+    ];
 
     /**
      * @var Accept
@@ -27,48 +39,68 @@ class HeaderAccept
     private $media;
 
 
+    /**
+     * HeaderAccept constructor.
+     * @param null $availableContentTypes
+     */
     public function __construct($availableContentTypes = null)
     {
         $server = $_SERVER;
         // Drasko: dont analize media type if a file extension exists in the path
         unset($server['REQUEST_URI']);
 
-        $acceptFactory = new AcceptFactory($server);
-        $this->accept = $acceptFactory->newInstance();
-        $this->availableContentTypes = $availableContentTypes;
+        $acceptFactory                  = new AcceptFactory($server);
+        $this->accept                   = $acceptFactory->newInstance();
+        $this->availableContentTypes    = $availableContentTypes;
+        $this->media                    = null;
     }
 
+    /**
+     * @return string
+     */
     public function getFormat()
     {
         $this->media = $this->accept->negotiateMedia($this->available());
-        return $this->hasDetected()
-            ? $this->acceptMap()[$this->media->getValue()]
-            : Format::JSON;
+
+        return $this->hasDetected() && $this->existInAcceptMap()
+            ? $this->getFormatFromAcceptMap()
+            : self::FORMAT_JSON;
+    }
+
+    /**
+     * @return bool
+     */
+    private function existInAcceptMap()
+    {
+        return array_key_exists($this->media->getValue(), self::ACCEPT_MAP);
     }
 
     /**
      * @return array
      */
-    private function acceptMap()
-    {
-        return [
-            HeaderAcceptConst::JSON => Format::JSON,
-            HeaderAcceptConst::HTML => Format::TWIG,
-        ];
-    }
-
     private function available()
     {
-        return $this->availableContentTypes
+        return is_array($this->availableContentTypes)
             ? $this->availableContentTypes
             : [
-                HeaderAcceptConst::JSON,
-                HeaderAcceptConst::HTML,
+                self::ACCEPT_JSON,
+                self::ACCEPT_HTML,
             ];
     }
 
+    /**
+     * @return bool
+     */
     private function hasDetected()
     {
         return $this->media instanceof MediaValue;
+    }
+
+    /**
+     * @return string
+     */
+    private function getFormatFromAcceptMap()
+    {
+        return self::ACCEPT_MAP[$this->media->getValue()];
     }
 }
